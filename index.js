bce7361edd93e0a066811d700fefaa4c1b6631c6
Rhys Wicks <brothersgreenrox@gmail.com>
2021-01-15 15:17:24 +1000

    Remove book functionality

--- index.js
+++ index.js
@@ -42,10 +42,11 @@ document.querySelector("#new_movie").addEventListener("submit", function (e) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  showBook(book);
+  const index = myLibrary.indexOf(book);
+  showBook(book, index);
 }
 
-function showBook(book) {
+function showBook(book, index) {
   const col = document.createElement('div');
   const card = document.createElement('div');
   const card_title = document.createElement('div');
@@ -54,11 +55,14 @@ function showBook(book) {
   const p = document.createElement('p');
   const p_two = document.createElement('p');
   const p_three = document.createElement('p');
+  const a = document.createElement('a');
+  const i = document.createElement('i');
 
   span.textContent = book.title;
   p.textContent = book.author;
   p_two.textContent = book.pages;
   p_three.textContent = book.read;
+  i.textContent = "delete";
 
   row.classList.add('row');
   col.classList.add('col', 'l3');
@@ -69,13 +73,46 @@ function showBook(book) {
   p.classList.add('black-text');
   p_two.classList.add('black-text');
   p_three.classList.add('black-text');
+  a.classList.add('btn-floating', 'btn-medium', 'waves-effect', 'waves-light', 'red', 'remove');
+  i.classList.add('material-icons');
+  a.style.float = "right";
 
   row.appendChild(col);
   col.appendChild(card);
+  card_title.appendChild(a);
   card.appendChild(card_title);
   card_title.appendChild(span);
   card.appendChild(card_content);
   card_content.appendChild(p);
   card_content.appendChild(p_two);
   card_content.appendChild(p_three);
+
+  a.appendChild(i);
+
+  col.dataset.index = index;
+
+  document.querySelectorAll('.remove')[col.dataset.index].addEventListener('click', () => {
+    removeBook(col.dataset.index);
+    updateBookIndex();
+  });
+}
+
+
+function updateBookIndex() {
+  let x = 0;
+  let thecol = document.querySelectorAll('.col.l3');
+  thecol.forEach(c => {
+    c.dataset.index = x;
+    x++;
+  });
+}
+
+
+function removeBook(index) {
+  console.log(index);
+  let x = 0;
+  let col = document.querySelectorAll('.col.l3');
+  myLibrary.splice(index, 1);
+  document.querySelector(`[data-index='${index}']`).remove();
+  console.log(myLibrary);
 }
