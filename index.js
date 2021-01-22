d72f80be268a47aaba0bb9e5c1ccaa5c16f725bd
Rhys Wicks <brothersgreenrox@gmail.com>
2021-01-22 15:08:49 +1000

    Books can now be removed from local storage

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+"use strict";
 let myLibrary = [];
 const row = document.createElement('div');
 const container = document.querySelector('.container');
 container.appendChild(row);
+
+if (getStorage()) {
+  myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   let elems = document.querySelectorAll('.modal');
   M.Modal.init(elems);
 });
 
-
 document.addEventListener('DOMContentLoaded', function () {
   let elems = document.querySelectorAll('select');
   M.FormSelect.init(elems);
 });
-
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -44,68 +47,77 @@ document.querySelector("#new_movie").addEventListener("submit", function (e) {
   saveLibrary()
 })
 
-
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  const index = myLibrary.indexOf(book);
-  showBook(book, index);
+  showBooks()
 }
 
-function showBook(book, index) {
-  const col = document.createElement('div');
-  const card = document.createElement('div');
-  const card_title = document.createElement('div');
-  const card_content = document.createElement('div');
-  const span = document.createElement('span');
-  const p = document.createElement('p');
-  const p_two = document.createElement('p');
-  const book_status = document.createElement('a');
-  const a = document.createElement('a');
-  const i = document.createElement('i');
 
-  span.textContent = book.title;
-  p.textContent = book.author;
-  p_two.textContent = book.pages;
-  book_status.textContent = book.read;
-  i.textContent = "delete";
+function showBooks() {
+  const items = document.querySelectorAll('.col.l3');
 
-  row.classList.add('row');
-  col.classList.add('col', 'l3', 's12');
-  card.classList.add('card');
-  card_title.classList.add('card-content', 'white');
-  card_content.classList.add('card-content');
-  span.classList.add('black-text', 'card-title');
-  p.classList.add('black-text');
-  p_two.classList.add('black-text');
-  book_status.classList.add('waves-effect', 'waves-light', 'btn', 'status');
-  a.classList.add('btn-floating', 'btn-medium', 'waves-effect', 'waves-light', 'red', 'remove');
-  i.classList.add('material-icons');
-  a.style.float = "right";
+  if (items.length > 0) {
+    items.forEach(item => item.remove());
+  }
 
-  row.appendChild(col);
-  col.appendChild(card);
-  card_title.appendChild(a);
-  card.appendChild(card_title);
-  card_title.appendChild(span);
-  card.appendChild(card_content);
-  card_content.appendChild(p);
-  card_content.appendChild(p_two);
-  card_content.appendChild (book_status);
+  let index = 0
+  myLibrary.forEach(book => {
 
-  a.appendChild(i);
+    const col = document.createElement('div');
+    const card = document.createElement('div');
+    const card_title = document.createElement('div');
+    const card_content = document.createElement('div');
+    const span = document.createElement('span');
+    const p = document.createElement('p');
+    const p_two = document.createElement('p');
+    const book_status = document.createElement('a');
+    const a = document.createElement('a');
+    const i = document.createElement('i');
 
-  col.dataset.index = index;
-
-  document.querySelectorAll('.remove')[col.dataset.index].addEventListener('click', () => {
-    removeBook(col.dataset.index);
-    updateBookIndex();
-  });
-
-  document.querySelectorAll('.status')[col.dataset.index].addEventListener('click', () => {
-    updateBookIndex();
-    book.status();
+    span.textContent = book.title;
+    p.textContent = book.author;
+    p_two.textContent = book.pages;
     book_status.textContent = book.read;
-    saveLibrary()
+    i.textContent = "delete";
+
+    row.classList.add('row');
+    col.classList.add('col', 'l3', 's12');
+    card.classList.add('card');
+    card_title.classList.add('card-content', 'white');
+    card_content.classList.add('card-content');
+    span.classList.add('black-text', 'card-title');
+    p.classList.add('black-text');
+    p_two.classList.add('black-text');
+    book_status.classList.add('waves-effect', 'waves-light', 'btn', 'status');
+    a.classList.add('btn-floating', 'btn-medium', 'waves-effect', 'waves-light', 'red', 'remove');
+    i.classList.add('material-icons');
+    a.style.float = "right";
+
+    row.appendChild(col);
+    col.appendChild(card);
+    card_title.appendChild(a);
+    card.appendChild(card_title);
+    card_title.appendChild(span);
+    card.appendChild(card_content);
+    card_content.appendChild(p);
+    card_content.appendChild(p_two);
+    card_content.appendChild(book_status);
+
+    a.appendChild(i);
+    col.dataset.index = index;
+
+    document.querySelectorAll('.remove')[index].addEventListener('click', () => {
+      removeBook(col.dataset.index);
+      updateBookIndex();
+    });
+
+    document.querySelectorAll('.status')[index].addEventListener('click', () => {
+      updateBookIndex();
+      book.status();
+      book_status.textContent = book.read;
+      saveLibrary()
+    });
+    index++;
   });
 }
 
@@ -119,16 +131,22 @@ function updateBookIndex() {
   });
 }
 
-
 function removeBook(index) {
   console.log(index);
   myLibrary.splice(index, 1);
   document.querySelector(`[data-index='${index}']`).remove();
   console.log(myLibrary);
+  saveLibrary();
 }
 
 function saveLibrary() {
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
+function getStorage() {
+  let data = JSON.parse(localStorage.getItem('myLibrary'));
+  return data;
+}
+
+showBooks();
 
